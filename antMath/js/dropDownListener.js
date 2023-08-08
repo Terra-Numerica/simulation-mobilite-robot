03aba4a806f0685a43d1dac4336f8120f38513ad
Raysharo <lucaslyonbusiness@gmail.com>
2023-08-08 10:47:15 +0200

    new try event listener

--- antMath/js/dropDownListener.js
+++ antMath/js/dropDownListener.js
@@ -1,34 +1,36 @@
-function dropDownListener() {
+function dropDownListener1() {
   const backgroundPickerDiv = document.getElementById('background-picker-div');
   const langPicker = document.getElementById('lang-picker');
 
-  document.getElementById('icon-background-picker').addEventListener('touchend', (event) => {
-    event.stopPropagation();
+  document.getElementById('icon-background-picker').addEventListener('touchend', () => {
+    console.log('icon-background-picker touchend: Opening backgroundPickerDiv');
     toggleDropdown(backgroundPickerDiv);
   });
 
-  document.getElementById('icon-lang-picker').addEventListener('touchend', (event) => {
-    event.stopPropagation();
+  document.getElementById('icon-lang-picker').addEventListener('touchend', () => {
+    console.log('icon-lang-picker touchend: Opening langPicker');
     toggleDropdown(langPicker);
   });
 
-  document.getElementById('background-picker-div').addEventListener('mouseenter', (event) => {
-    event.stopPropagation();
+  document.getElementById('background-picker-div').addEventListener('mouseenter', () => {
+    console.log('background-picker-div mouseenter: Opening backgroundPickerDiv');
     toggleDropdown(backgroundPickerDiv);
   });
 
-  document.getElementById('lang-picker').addEventListener('mouseenter', (event) => {
-    event.stopPropagation();
+  document.getElementById('lang-picker').addEventListener('mouseenter', () => {
+    console.log('lang-picker mouseenter: Opening langPicker');
     toggleDropdown(langPicker);
   });
 
   document.getElementById('background-picker-div').addEventListener('mouseleave', (event) => {
-    event.stopPropagation();
+    console.log('background-picker-div mouseleave: Closing backgroundPickerDiv');
+    // event.stopPropagation();
     backgroundPickerDiv.classList.remove('open');
   });
 
   document.getElementById('lang-picker').addEventListener('mouseleave', (event) => {
-    event.stopPropagation();
+    console.log('lang-picker mouseleave: Closing langPicker');
+    // event.stopPropagation();
     langPicker.classList.remove('open');
   });
 
@@ -37,101 +39,143 @@ function dropDownListener() {
 
   langInputs.forEach((input) => {
     input.addEventListener('click', (event) => {
-      event.stopPropagation();
+      console.log('line-lang-picker input click: Closing langPicker');
+      // event.stopPropagation();
       langPicker.classList.remove('open');
     });
   });
 
   backgroundInputs.forEach((input) => {
     input.addEventListener('click', (event) => {
-      event.stopPropagation();
+      console.log('line-background-picker input click: Closing backgroundPickerDiv');
+      // event.stopPropagation();
       backgroundPickerDiv.classList.remove('open');
     });
   });
 
-  window.addEventListener('click', (event) => {
-    if (!backgroundPickerDiv.contains(event.target)) {
-      backgroundPickerDiv.classList.remove('open');
-    }
-    if (!langPicker.contains(event.target)) {
-      langPicker.classList.remove('open');
-    }
-  });
+  // window.addEventListener('click', (event) => {
+  //   console.log('window click: Closing dropdowns');
+  //   if (!backgroundPickerDiv.contains(event.target)) {
+  //     backgroundPickerDiv.classList.remove('open');
+  //   }
+  //   if (!langPicker.contains(event.target)) {
+  //     langPicker.classList.remove('open');
+  //   }
+  // });
 
-  window.addEventListener('touchend', (event) => {
-    if (!backgroundPickerDiv.contains(event.target)) {
-      backgroundPickerDiv.classList.remove('open');
-    }
-    if (!langPicker.contains(event.target)) {
-      langPicker.classList.remove('open');
-    }
-  });
+  // window.addEventListener('touchend', (event) => {
+  //   console.log('window touchend: Closing dropdowns');
+  //   if (!backgroundPickerDiv.contains(event.target)) {
+  //     backgroundPickerDiv.classList.remove('open');
+  //   }
+  //   if (!langPicker.contains(event.target)) {
+  //     langPicker.classList.remove('open');
+  //   }
+  // });
 
-  function toggleDropdown(dropdown) {
-    dropdown.classList.toggle('open');
-  }
+  
 }
 
 
+function dropDownListener(){
+  const backgroundPickerDiv = document.getElementById('background-picker-div');
+  const langPicker = document.getElementById('lang-picker');
+
+  // whent touchend on icon-background-picker, toggle backgroundPickerDiv and close langPicker
+  document.getElementById('icon-background-picker').addEventListener('touchend', () => {
+    console.log('toggle backgroundPickerDiv and close langPicker');
+    toggleDropdown(backgroundPickerDiv);
+    langPicker.classList.remove('open');
+  });
+
+  // whent touchend on icon-lang-picker, toggle langPicker and close backgroundPickerDiv
+  document.getElementById('icon-lang-picker').addEventListener('touchend', () => {
+    console.log('toggle langPicker and close backgroundPickerDiv');
+    toggleDropdown(langPicker);
+    backgroundPickerDiv.classList.remove('open');
+  });
+
+  // when mouseenter on background-picker-div, open backgroundPickerDiv and close langPicker
+  document.getElementById('background-picker-div').addEventListener('mouseenter', () => {
+    console.log('open backgroundPickerDiv and close langPicker');
+    toggleDropdown(backgroundPickerDiv);
+    langPicker.classList.remove('open');
+  } );
+
+  // when mouseenter on lang-picker, open langPicker and close backgroundPickerDiv
+  document.getElementById('lang-picker').addEventListener('mouseenter', () => {
+    console.log('open langPicker and close backgroundPickerDiv');
+    toggleDropdown(langPicker);
+    backgroundPickerDiv.classList.remove('open');
+  }
+  );
+
+  // when mouseleave on background-picker-div, close backgroundPickerDiv
+  document.getElementById('background-picker-div').addEventListener('mouseleave', (event) => {
+    console.log('close backgroundPickerDiv');
+    // event.stopPropagation();
+    backgroundPickerDiv.classList.remove('open');
+  }
+  );
+
+  // when mouseleave on lang-picker, close langPicker
+  document.getElementById('lang-picker').addEventListener('mouseleave', (event) => {
+    console.log('lang-picker mouseleave: Closing langPicker');
+    // event.stopPropagation();
+    langPicker.classList.remove('open');
+  }
+  );
+
+  // when click on lang-picker input, close langPicker
+  const langInputs = document.querySelectorAll('input[name="line-lang-picker"]');
+  langInputs.forEach((input) => {
+    input.addEventListener('click', (event) => {
+      console.log('line-lang-picker input click: Closing langPicker');
+      // event.stopPropagation();
+      langPicker.classList.remove('open');
+    });
+  }
+
+  );
+
+  // when click on background-picker-div input, close backgroundPickerDiv
+  const backgroundInputs = document.querySelectorAll('input[name="line-background-picker"]');
+  backgroundInputs.forEach((input) => {
+    input.addEventListener('click', (event) => {
+      console.log('line-background-picker input click: Closing backgroundPickerDiv');
+      // event.stopPropagation();
+      backgroundPickerDiv.classList.remove('open');
+    });
+  } );
+
+  // when click on window, close dropdowns
+  // useless because mouseleave is enough
+  // window.addEventListener('click', (event) => {
+  //   console.log('window click: Closing dropdowns');
+  //   if (!backgroundPickerDiv.contains(event.target)) {
+  //     backgroundPickerDiv.classList.remove('open');
+  //   }
+  //   if (!langPicker.contains(event.target)) {
+  //     langPicker.classList.remove('open');
+  //   }
+  // } );
+
+  // when touchend on window, close dropdowns
+  // remove to avoid double event
+  // window.addEventListener('touchend', (event) => {
+  //   console.log('window touchend: Closing dropdowns');
+  //   if (!backgroundPickerDiv.contains(event.target)) {
+  //     backgroundPickerDiv.classList.remove('open');
+  //   }
+  //   if (!langPicker.contains(event.target)) {
+  //     langPicker.classList.remove('open');
+  //   }
+  // }
+  // );
+
+}
 
 
-
-
-// // On définit une fonction pour ajouter la classe "hovered" lorsqu'un élément est survolé
-// function addHoverClass(event) {
-//     const target = event.target;
-//     target.classList.add("hovered");
-// }
-
-// // On définit une fonction pour retirer la classe "hovered" lorsque le pointeur quitte un élément
-// function removeHoverClass(event) {
-//     const target = event.target;
-//     target.classList.remove("hovered");
-// }
-
-// // On ajoute les événements "mouseenter" et "mouseleave" à tous les éléments ayant la classe "dropdown-square"
-// const dropdownSquares = document.querySelectorAll(".dropdown-square");
-// dropdownSquares.forEach((dropdownSquare) => {
-//     dropdownSquare.addEventListener("mouseenter", addHoverClass);
-//     dropdownSquare.addEventListener("mouseleave", removeHoverClass);
-// });
-
-// // On ajoute les événements "mouseenter" et "mouseleave" à l'élément ayant l'id "background-picker-div"
-// const backgroundPicker = document.getElementById("background-picker-div");
-// backgroundPicker.addEventListener("mouseenter", function () {
-//     this.style.height = `calc(var(--nbBGChoice) * var(--iconHeight))`;
-//     this.style.minHeight = `calc(var(--nbBGChoice) * var(--minIconHeight))`;
-// });
-// backgroundPicker.addEventListener("mouseleave", function () {
-//     this.style.height = `var(--iconHeight)`;
-//     this.style.minHeight = `var(--minIconHeight)`;
-// });
-
-// // On ajoute les événements "mouseenter" et "mouseleave" à l'élément ayant l'id "lang-picker"
-// const langPicker = document.getElementById("lang-picker");
-// langPicker.addEventListener("mouseenter", function () {
-//     this.style.height = `calc(var(--nbLangChoice) * var(--iconHeight))`;
-//     this.style.minHeight = `calc(var(--nbLangChoice) * var(--minIconHeight))`;
-// });
-// langPicker.addEventListener("mouseleave", function () {
-//     this.style.height = `var(--iconHeight)`;
-//     this.style.minHeight = `var(--minIconHeight)`;
-// });
-
-// // On récupère tous les éléments input ayant la classe "dropdown-square"
-// const dropdownSquareInputs = document.querySelectorAll(".dropdown-square input");
-
-// // On ajoute un événement "click" à chaque élément input
-// dropdownSquareInputs.forEach((input) => {
-//     input.addEventListener("click", function () {
-//         // On récupère le label associé à l'input cliqué
-//         const label = document.querySelector(`label[for="${this.id}"]`);
-//         if (this.checked) {
-//             // Si l'input est coché, on ajoute la classe "hovered" pour que le label soit visible
-//             label.classList.add("hovered");
-//         } else {
-//             // Sinon, on retire la classe "hovered" pour que le label soit caché
-//             label.classList.remove("hovered");
-//         }
-//     });
-// });
+function toggleDropdown(dropdown) {
+  dropdown.classList.toggle('open');
+}
