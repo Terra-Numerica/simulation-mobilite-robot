976830fafd5035867b378b491a4c276e2d26dafa
Raysharo <lucaslyonbusiness@gmail.com>
2023-08-08 14:36:36 +0200

    mini correction bug translation logoTN

--- antMath/js/langSwitch.js
+++ antMath/js/langSwitch.js
@@ -123,6 +123,8 @@ function switchLang2() {
             elt.title = (allowDownload) ? TRANSLATE[elt.id].title[language] : TRANSLATE.alertGenerateGIF.innerText[language];
         }
         else {
+            
+            console.log("elt", elt);
             elt.title = TRANSLATE[elt.id].title[language];
         }
         
